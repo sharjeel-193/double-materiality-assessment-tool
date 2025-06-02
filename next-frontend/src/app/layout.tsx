97629cfd45f63cd388aa68553ff4079fd2f7acda
Muhammad Sharjeel Maqsood <m.sharjeel193@gmail.com>
2025-06-02 13:19:13 +0200

    Set Up Apollo Client and Auth Provider

--- next-frontend/src/app/layout.tsx
+++ next-frontend/src/app/layout.tsx
@@ -1,15 +1,20 @@
 
-import { ThemeContextProvider } from '@/providers';
+import { ThemeContextProvider, AuthContextProvider } from '@/providers';
 // import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import React from 'react';
 import './global.css'
+import { ApolloWrapper } from '@/graphql/ApolloWrapper';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body>
                 <ThemeContextProvider>
-                    {children}
+                    <ApolloWrapper>
+                        <AuthContextProvider>
+                            {children}
+                        </AuthContextProvider>
+                    </ApolloWrapper>
                 </ThemeContextProvider>
             </body>
         </html>
---
+++ next-frontend/src/graphql/queries/index.ts
@@ -0,0 +1,5 @@
+export {
+    LOGIN_MUTATION,
+    GET_CURRENT_USER,
+    CREATE_USER_MUTATION
+} from './auth'
